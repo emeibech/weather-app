c3e1217f882df8c193f01028bfffa2df28ca2cff
emeibech <emeibech@gmail.com>
2023-01-10 01:59:52 +0800

    Add error handling to async functions

--- src/modules/weatherInfo.js
+++ src/modules/weatherInfo.js
@@ -4,27 +4,43 @@ const appID = 'a864b3057d366f0312e36cebd74c7077';
 const rawURL = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${appID}`;
 
 const fetchData = async (url) => {
-  const response = await fetch(url, { mode: 'cors' });
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(url, { mode: 'cors' });
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return console.log(err);
+  }
 };
 
 const geocodeURL = async (type) => {
-  const data = await fetchData(rawURL);
-  const geocodedURL = `https://api.openweathermap.org/data/2.5/${type}?lat=${data[0].lat}&lon=${data[0].lon}&cnt=&appid=${appID}`;
-  return geocodedURL;
+  try {
+    const data = await fetchData(rawURL);
+    const geocodedURL = `https://api.openweathermap.org/data/2.5/${type}?lat=${data[0].lat}&lon=${data[0].lon}&cnt=&appid=${appID}`;
+    return geocodedURL;
+  } catch (err) {
+    return console.log(err);
+  }
 };
 
 const getWeatherData = async () => {
-  const url = await geocodeURL('weather');
-  const data = await fetchData(url);
-  return data;
+  try {
+    const url = await geocodeURL('weather');
+    const data = await fetchData(url);
+    return data;
+  } catch (err) {
+    return console.log(err);
+  }
 };
 
 const getForecastData = async () => {
-  const url = await geocodeURL('forecast');
-  const data = await fetchData(url);
-  return data;
+  try {
+    const url = await geocodeURL('forecast');
+    const data = await fetchData(url);
+    return data;
+  } catch (err) {
+    return console.log(err);
+  }
 };
 
 const getWeatherInfo = async () => {
