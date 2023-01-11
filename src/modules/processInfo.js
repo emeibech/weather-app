74d4e93d60a77a862b6b86fbb3e3f50afcf953cd
emeibech <emeibech@gmail.com>
2023-01-12 02:52:01 +0800

    Refactor Capitalize function

--- src/modules/processInfo.js
+++ src/modules/processInfo.js
@@ -1,14 +1,20 @@
 import getCurrentWeatherInfo from './weatherInfo';
 
-/* It's just a function for capitalizing the description.
-  There's definitely a better way of doing this but I don't know it just yet. */
+/* This is just a function for capitalizing the description.
+  This version look a little bit better than the previous one.
+  The "text-transform: capitalize" in CSS seems better and easier, though,
+  but I'll stick with this in case I need it in the future. */
 const capitalize = (description) => {
-  const [first, second] = description;
-  const arr1 = first.split('');
-  arr1[0] = arr1[0].toUpperCase();
-  const arr2 = second.split('');
-  arr2[0] = arr2[0].toUpperCase();
-  return [arr1.join(''), arr2.join('')].join(' ');
+  const returnValue = [];
+
+  description.forEach((item) => {
+    const word = (`${item}`);
+    const firstLetter = word.charAt(0).toUpperCase();
+    const remainingLetters = word.slice(1);
+    returnValue.push(`${firstLetter}${remainingLetters}`);
+  });
+
+  return returnValue.join(' ');
 };
 
 const processInfo = async (location) => {
