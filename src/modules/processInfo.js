b78230624b39daf27f07a3c7ca648d7b477f276e
emeibech <emeibech@gmail.com>
2023-01-12 19:04:07 +0800

    Make weather info human-readable

--- src/modules/processInfo.js
+++ src/modules/processInfo.js
@@ -1,4 +1,5 @@
 import getCurrentWeatherInfo from './weatherInfo';
+import countries from '../countries.json';
 
 /* This is just a function for capitalizing the description.
   This version look a little bit better than the previous one.
@@ -17,9 +18,32 @@ const capitalize = (description) => {
   return returnValue.join(' ');
 };
 
+const getPercentage = (number) => `${number * 100}%`;
+
+const convertToCelsius = (kelvin) => `${Math.round(kelvin - 273.15)}°C`;
+
+const getReadableTimestamp = (unix) => {
+  const dataObj = new Date(unix * 1000);
+  const time = [dataObj.getHours(), dataObj.getMinutes()];
+  if (time[0] > 12) time.splice(0, 1, time[0] - 12);
+  return time.join(':');
+};
+
 const processInfo = async (location) => {
   const info = await getCurrentWeatherInfo(location);
   info.description = capitalize(info.description.split(' '));
+  info.pop = getPercentage(info.pop);
+  info.humidity = `${info.humidity}%`;
+  info.visibility = `${info.visibility / 1000} km`;
+  info.temp = convertToCelsius(info.temp);
+  info.feelsLike = convertToCelsius(info.feelsLike);
+  info.tempMin = convertToCelsius(info.tempMin);
+  info.tempMax = convertToCelsius(info.tempMax);
+  info.windSpeed = `${Math.round(info.windSpeed * (18 / 5))} km/h`;
+  info.cloudCover = `${info.cloudCover}%`;
+  info.sunrise = `${getReadableTimestamp(info.sunrise)} AM`;
+  info.sunset = `${getReadableTimestamp(info.sunset)} PM`;
+  info.country = countries[info.country];
 
   console.log(info);
 
