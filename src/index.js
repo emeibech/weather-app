9361301a90ef096bbf306f2e36ce4d5887b1aa2d
emeibech <emeibech@gmail.com>
2023-01-15 05:40:22 +0800

    Add more functionality to search bar

--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ import renderWeatherInfo from './modules/renderWeatherInfo';
 import { getClientLocation } from './modules/clientLocation';
 import searchWhite from './assets/search-svg/search-white.svg';
 import searchDefault from './assets/search-svg/search-default.svg';
+import clearSVG from './assets/search-svg/clear-white.svg';
 
 const search = document.querySelector('#location');
 const imgSearch = document.querySelector('[data-search-svg]');
+const clear = document.querySelector('[data-clear]');
 
 document.addEventListener('DOMContentLoaded', async () => {
   const location = await getClientLocation();
   renderWeatherInfo(location);
+  search.value = '';
 });
 
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault();
-  const input = document.querySelector('#location');
-  const location = input.value;
+  const location = search.value;
   renderWeatherInfo(location);
-  input.value = '';
-  input.placeholder = 'Searching...';
+  search.placeholder = 'Searching...';
+  search.value = '';
+  clear.src = '';
+  search.blur();
 });
 
 search.addEventListener('focus', () => {
@@ -27,4 +31,13 @@ search.addEventListener('focus', () => {
 
 search.addEventListener('blur', () => {
   imgSearch.src = searchDefault;
+});
+
+search.addEventListener('input', () => {
+  clear.src = clearSVG;
+  clear.addEventListener('mouseup', () => {
+    search.value = '';
+    clear.src = '';
+    search.focus();
+  });
 });
